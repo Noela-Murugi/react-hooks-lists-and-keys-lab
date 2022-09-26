48e8ea74cf0e0122f721939f893a544046208763
Noela-Murugi <murugin254@gmail.com>
2022-09-26 13:26:25 +0300

    completed the assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const unqprop = links.map(unqprop =>
+    <a key={unqprop} href = {`#${unqprop}`}>{unqprop}</a>)
+  return <nav>{
+    /* display an <a> tag for each link here */
+    unqprop
+    }</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {
+        /* render a <span> for each technology in the technologies array */
+        technologies.map((item,proj)=>{
+          return <span key={proj}>{item}</span>
+        })
+      }
       </div>
     </div>
   );
